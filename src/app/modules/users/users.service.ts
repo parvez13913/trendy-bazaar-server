a9d55705c48a15eb429ea1971626bdaee52fadfc
parvez <parvezz13913@gmail.com>
2024-10-26 09:04:34 +0600

    [DONE] Password hash

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,8 +1,19 @@
 import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import config from "../../../config";
 import { prisma } from "../../../shard/prisma";
 
 const createUser = async (data: User): Promise<User | null> => {
-  const result = await prisma.user.create({ data });
+  const hashedPassword = await bcrypt.hash(
+    data?.password,
+    Number(config.bcrypt_salt_round)
+  );
+  const result = await prisma.user.create({
+    data: {
+      ...data,
+      password: hashedPassword,
+    },
+  });
   return result;
 };
 
