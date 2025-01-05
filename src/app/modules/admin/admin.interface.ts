798ef38cc8056f075f8e54bfd0ca347117c7e50c
parvez <parvezz13913@gmail.com>
2025-01-05 19:18:01 +0600

    Updated the code

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -3,3 +3,18 @@ export interface IAdminFilters {
   email?: string;
   contactNo?: string;
 }
+
+export interface IAdmin {
+  id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  gender: string;
+  address: string;
+  email: string;
+  contactNo: string;
+  dateOfBirth: string;
+  bloodGroup: string;
+  createdAt: string;
+  updatedAt: string;
+}
